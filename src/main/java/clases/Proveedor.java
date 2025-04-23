dea678842aca7af9f107ef5e45aa27956410a8cb
Sebastian <derlis567y@gmail.com>
2025-04-23 10:14:01 -0500

    FEAT (UPS-004): "Implementacion de clase Empleado con herencia a la clase Persona"

--- src/main/java/clases/Proveedor.java
+++ src/main/java/clases/Proveedor.java
@@ -3,5 +3,13 @@ package clases;
 import java.util.List;
 
 public class Proveedor extends Persona{
+    private int idProveedor;
+    private String ruc;
+    private List<Producto> productos;
+
+    public Proveedor(String nombre, String apellido, String identificacion, String telefono, String correoElectronico) {
+        super(nombre, apellido, identificacion, telefono, correoElectronico);
+    }
+
 
 }
