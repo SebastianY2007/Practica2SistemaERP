651c9a80fff8dbfc0caeeeafb5ebcc0ffc3a07e0
Javier <javierbarrezueta1@gmail.com>
2025-04-24 07:21:09 -0500

    FEAT (UPS-002) "Implementacion de la clase Producto con sus Constructores, Getters and Setters y el ToString"

---
+++ src/main/java/clases/Producto.java
@@ -0,0 +1,79 @@
+package clases;
+
+public class Producto {
+    private int codigo;
+    private String nombre;
+    private String descripcion;
+    private double precio;
+    private String categoria;
+    private int stock;
+
+    public Producto(int codigo, String nombre, String descripcion, double precio, String categoria, int stock) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.precio = precio;
+        this.categoria = categoria;
+        this.stock = stock;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    @Override
+    public String toString() {
+        return "Producto{" +
+                "codigo=" + codigo +
+                ", nombre='" + nombre + '\'' +
+                ", descripcion='" + descripcion + '\'' +
+                ", precio=" + precio +
+                ", categoria='" + categoria + '\'' +
+                ", stock=" + stock +
+                '}';
+    }
+}
