5ce16582586fad8daaabdaef7a530340aafd5f83
Javier <javierbarrezueta1@gmail.com>
2025-05-24 00:21:56 -0500

    FEAT (UPS-003): "Creacion de la clase PersonaView con un boton para agregar una persona y otro boton para pedir la lista de personas agregadas"

--- src/main/java/ec/edu/ups/poo/menu/App.java
+++ src/main/java/ec/edu/ups/poo/menu/App.java
@@ -4,7 +4,6 @@ import ec.edu.ups.poo.menu.PersonaView;
 
 public class App {
     public static void main(String[] args) {
-        // Inicia la ventana de PersonaView
         new PersonaView();
     }
 }
